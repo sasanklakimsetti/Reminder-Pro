ede0e020006b3da8e8846614687ce88698220fc4
Sasank Lakimsetti <saisasank.lakimsetti@gmail.com>
2025-04-24 17:12:50 +0530

    Not working on alarms, just implementing reminder

--- app/src/main/java/com/sasank/reminderpro/MainActivity.kt
+++ app/src/main/java/com/sasank/reminderpro/MainActivity.kt
@@ -3,10 +3,9 @@ package com.sasank.reminderpro
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sasank.reminderpro.ui.alarm.AlarmFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/sasank/reminderpro/receiver/AlarmReceiver.kt
+++
@@ -1,4 +0,0 @@
-package com.sasank.reminderpro.receiver
-
-class AlarmReceiver {
-}
