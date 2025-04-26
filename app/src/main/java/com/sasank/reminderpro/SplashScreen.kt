2bc81a2658d82a9f98d358019b2f93f8e3b7d4b0
Sasank Lakimsetti <saisasank.lakimsetti@gmail.com>
2025-04-26 15:51:14 +0530

    changed the app logo and launcher icon, added typing animation for app name during SplashScreen

--- app/src/main/java/com/sasank/reminderpro/SplashScreen.kt
+++ app/src/main/java/com/sasank/reminderpro/SplashScreen.kt
@@ -2,19 +2,35 @@ package com.sasank.reminderpro
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.sasank.reminderpro.MainActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        android.os.Handler().postDelayed({
+
+        val titleTextView: TextView = findViewById(R.id.textView1)
+        val title = "ReminderPro"
+
+        val displayedText = StringBuilder()
+
+        // Create a handler to introduce delays between each character update
+        val handler = Handler(mainLooper)
+
+        // Use a loop with a delay to show each character one by one
+        for (i in 0 until title.length) {
+            handler.postDelayed({
+                displayedText.append(title[i])
+                titleTextView.text = displayedText.toString()
+            }, i * 100L) // 200ms delay between each character
+        }
+
+        // After 5 seconds, start the MainActivity
+        Handler(mainLooper).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        },5000)
+        }, 1500)
     }
 }
